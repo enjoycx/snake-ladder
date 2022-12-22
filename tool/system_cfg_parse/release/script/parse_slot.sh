01637c66653a8202c3fc02aa8d23bc99498d0b8e
王晨曦 <chenxi.wang@senturygame.com>
2022-12-22 12:23:26 +0800

    delete

--- tool/system_cfg_parse/release/script/parse_slot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-slotOutGoPath=../../../../controller/spin/controller/config/
-slotOutJsonPath=../../../../assets/slot/
-for i in `ls ../../../../../config_slots/${2}/ | grep '^slot*'`;
-    do ${1} -s=false -f ../../../../../config_slots/${2}/${i} -ogp ${slotOutGoPath} -ojp ${slotOutJsonPath}
-done
--- tool/system_cfg_parse/run_online.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sourcePath=../../../config_system/Online_Config/
-outJsonPath=../../assets/
-outGoPath=../../config/
-./linux/system_cfg_parse -sp $sourcePath -ojp $outJsonPath -ogp $outGoPath
-
-bash ./parse_slot_online.sh ./linux/system_cfg_parse
